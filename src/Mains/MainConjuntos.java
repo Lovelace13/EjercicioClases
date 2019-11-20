c9a9ad8848b621e21d56fce199b2aa26dc11a966
Katiuska Marín <kmmarin@espol.edu.ec>
2019-11-20 03:27:47 -0500

    Creacion de Main para comprobar tda conjuntos

--- src/Mains/MainConjuntos.java
+++ src/Mains/MainConjuntos.java
@@ -15,26 +15,66 @@ public class MainConjuntos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         /**
-         * Conjunto de numeros pares menores que 10
+         * Comparador de enteros.
          */
-        ListSet<Integer> ConjuntoNumPares = new ListSet<Integer>(new Comparator<Integer>(){
+        Comparator<Integer> Enteros = new Comparator<Integer>(){
             @Override
             public int compare(Integer t1, Integer t2) {
                 return Integer.compare(t1, t2);
-            }
-            
-        });
+            }           
+        };
         
+        /**
+         * Conjunto de numeros pares menores que 10
+         */
+        ListSet<Integer> ConjuntoNumPares = new ListSet<Integer>(Enteros);
+        
+        ListSet<Integer> ConjuntoNumeros = new ListSet<Integer>(Enteros);
+        ListSet<Integer> ConjuntoIntersecion;
+        ListSet<Integer> DiferenciaConjunto;
+        ListSet<Integer> ConjuntoUnion;
+                
         ConjuntoNumPares.add(2);
         ConjuntoNumPares.add(8);
         ConjuntoNumPares.add(4);
         ConjuntoNumPares.add(6);
         
+        System.out.print("Conjunto NumerosPares: ");
         System.out.println(ConjuntoNumPares.toString());
 
-        ConjuntoNumPares.remove(3);
-        
+        ConjuntoNumPares.remove(2);
+        System.out.print("Conjunto NumerosPares sin el elemento '2': ");
         System.out.println(ConjuntoNumPares.toString());
+             
+        ConjuntoNumeros.add(1);
+        ConjuntoNumeros.add(3);
+        ConjuntoNumeros.add(4);
+        ConjuntoNumeros.add(8);
+        
+        ListSet<Integer> ConjuntoEspejo = ConjuntoNumeros;
+        
+        System.out.print("Conjunto Numeros: ");
+        System.out.println(ConjuntoNumeros.toString());
+        
+        ConjuntoIntersecion = ConjuntoNumPares.interseccion(ConjuntoNumeros);
+        System.out.print("Interseccion entre Conjunto Numeros y NumerosPares: ");
+        System.out.println(ConjuntoIntersecion.toString());
+        
+        DiferenciaConjunto = ConjuntoNumeros.diferencia(ConjuntoNumPares);
+        System.out.print("Diferencia entre Conjunto Numeros y NumerosPares: ");
+        System.out.println(DiferenciaConjunto.toString());
+        
+        ConjuntoUnion = ConjuntoNumeros.union(ConjuntoNumPares);
+        System.out.print("Union entre Conjunto Numeros y NumerosPares: ");
+        System.out.println(ConjuntoUnion.toString());
+        
+        ConjuntoNumPares.add(2);
+        
+        System.out.print("Compara dos conjuntos: ");
+        System.out.println(ConjuntoNumeros.equals(ConjuntoEspejo));
+        System.out.println(ConjuntoNumeros.equals(ConjuntoUnion));
+        System.out.println(ConjuntoIntersecion.equals(DiferenciaConjunto));
     }
 }
