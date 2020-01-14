d47a3a442a504b9132b8df439f83973da3cd9976
Katiuska Marín <kmmarin@espol.edu.ec>
2020-01-14 05:04:51 -0500

    main grafos

--- src/Mains/MainGrafos.java
+++ src/Mains/MainGrafos.java
@@ -5,6 +5,7 @@
  */
 package Mains;
 import Grafos.GraphLA;
+import Grafos.GraphMA;
 
 
 /**
@@ -26,6 +27,24 @@ public class MainGrafos {
         grafo.addEdge(7, 9, 2);
         
         System.out.println(grafo.toString());
+        
+        //GRAPHMA
+        System.out.println("\nGRAFO MA");
+        GraphMA<Integer> grafoMA = new GraphMA<>(true);
+        grafoMA.addVertex(1);
+        grafoMA.addVertex(2);
+        grafoMA.addVertex(3);
+        grafoMA.addVertex(4);
+        grafoMA.addVertex(5);
+        grafoMA.addEdge(2,1,6);
+        grafoMA.addEdge(5,1,6);
+        grafoMA.addEdge(2,4,6);
+        grafoMA.addEdge(4,2,6);
+        grafoMA.addEdge(2,3,6);
+        grafoMA.addEdge(4,6,2);
+        grafoMA.addEdge(3,1,1);
+        grafoMA.addEdge(10,5,3);
+        grafoMA.toString();
     }
     
 }
